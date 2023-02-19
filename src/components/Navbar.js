a314b265127d25620b8e51e4a1058e68a4117d7e
Aditya <its.aditya0109raj@gmail.com>
2023-02-20 03:40:21 +0530

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Link, useLocation ,useNavigate} from "react-router-dom";
+
+const Navbar = (props) => {
+    let location = useLocation();
+    let navigate=useNavigate();
+
+    const handleLogout=() =>{
+        localStorage.removeItem('token');
+        navigate('/login');
+        props.showAlert("Logged Out successfully!!","success")
+    }
+
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">SafeNote</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link className={`nav-link ${location.pathname==="/"? "active": ""}`} aria-current="page" to="/">Home</Link>
+                        </li>
+                    </ul>
+                    {!localStorage.getItem('token')?<form className="d-flex"> 
+                    <Link className="btn btn-success mx-1" to="/login" role="button">Login</Link>
+                    <Link className="btn btn-danger mx-1" to="/signup" role="button">Signup</Link>
+                    </form>:<button className='btn btn-primary' onClick={handleLogout}>Logout</button>}
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
---
+++ src/components/Signup.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+
+const Signup = (props) => {
+    const [credentials, setCredentials] = useState({ name: "", email: "", password: "", cpassword: "" })
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const { name, email, password, cpassword } = credentials;
+        if (password === cpassword) {
+            const response = await fetch("http://localhost:5000/api/auth/createuser", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ name, email, password })
+            });
+            const json = await response.json()
+            if (json.success) {
+                // Save the auth token and redirect
+                localStorage.setItem('token', json.authtoken);
+                navigate("/");
+                props.showAlert("Account created successfully!!","success")
+            }
+            else {
+                props.showAlert("Invalid credentials!!","danger")
+            }
+        }
+        else{
+            props.showAlert("Password and Confirm Password do not match","danger");
+        }
+    }
+
+    const onChange = (e) => {
+        setCredentials({ ...credentials, [e.target.name]: e.target.value })
+    }
+
+    return (
+        <div className='my-3'>
+            <h3>Sign Up here</h3>
+            <form  onSubmit={handleSubmit} >
+                <div className="mb-3 my-3" >
+                    <label htmlFor="name" className="form-label">Name</label>
+                    <input type="text" className="form-control" value={credentials.name} onChange={onChange} id="name" name="name" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email address</label>
+                    <input type="email" className="form-control" value={credentials.email} onChange={onChange} id="email" name="email" aria-describedby="emailHelp" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input type="password" className="form-control" onChange={onChange} name="password" id="password" minLength={8} required />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="cpassword" className="form-label">Confirm Password</label>
+                    <input type="password" className="form-control" onChange={onChange} name="cpassword" id="cpassword" minLength={8} required />
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default Signup
